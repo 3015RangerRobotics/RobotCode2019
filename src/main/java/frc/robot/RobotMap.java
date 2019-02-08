55a9c14f02f5f2f629f58fac8d258673c0113688
Kayla Stein <kstein0119@gmail.com>
2019-02-08 18:26:59 -0500

    we forgot to save

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,13 +10,20 @@ public class RobotMap {
 	public static final double kPeriod = 0.01;
 	public static final double wheelBaseWidth = 2;
 
-	public static final int rightDriveMotors = 0;
-	public static final int leftDriveMotors = 1;
+	public static final int rightDriveMaster = 0;
+	public static final int rightDriveFollower1 = 0;
+	public static final int rightDriveFollower2 = 0;
+	public static final int leftDriveMaster = 0;
+	public static final int leftDriveFollower1 = 0;
+	public static final int leftDriveFollower2 = 0;
 
-	public static final int rightDriveEncoder1 = 0;
-	public static final int rightDriveEncoder2 = 1;
-	public static final int leftDriveEncoder1 = 2;
-	public static final int leftDriveEncoder2 = 3;
+	// public static final int rightDriveMotors = 0;
+	// public static final int leftDriveMotors = 1;
+
+	// public static final int rightDriveEncoder1 = 0;
+	// public static final int rightDriveEncoder2 = 1;
+	// public static final int leftDriveEncoder1 = 2;
+	// public static final int leftDriveEncoder2 = 3;
 
 	public static final int elevatorTalonSRX = 3;
 	public static final int elevatorBottomLimit = 4;
