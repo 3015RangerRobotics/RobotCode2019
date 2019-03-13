ebd8ed1a782c0ab731c8a77e02ccba644afa1369
Kayla Stein <kstein0119@gmail.com>
2019-03-13 16:39:18 -0400

    Fixing autos and changing it so bronson can't make a big oopsie

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -9,15 +9,15 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 
 public class OI {
 	// Buttons for driver
-	XboxController driver = new XboxController(0);
+	static XboxController driver = new XboxController(0);
 	Button driverA1 = new JoystickButton(driver, 1);
 	Button driverB2 = new JoystickButton(driver, 2);
 	Button driverX3 = new JoystickButton(driver, 3);
 	Button driverY4 = new JoystickButton(driver, 4);
 	Button driverLB5 = new JoystickButton(driver, 5);
 	Button driverRB6 = new JoystickButton(driver, 6);
-	Button driverSEL7 = new JoystickButton(driver, 7);
-	Button driverSTART8 = new JoystickButton(driver, 8);
+	static Button driverSEL7 = new JoystickButton(driver, 7);
+	static Button driverSTART8 = new JoystickButton(driver, 8);
 	Button driverLS9 = new JoystickButton(driver, 9);
 	Button driverRS10 = new JoystickButton(driver, 10);
 	Button driverDLeft = new DPadButton(driver, DPadButton.Value.kDPadLeft);
@@ -25,19 +25,19 @@ public class OI {
 	Button driverDDown = new DPadButton(driver, DPadButton.Value.kDPadDown);
 	Button driverDRight = new DPadButton(driver, DPadButton.Value.kDPadRight);
 	Button driverLTrigger = new TriggerButton(driver, Hand.kLeft);
-	Button driverRTrigger = new TriggerButton(driver, Hand.kRight);
-	Button driverStartSelect = new DoubleButton(driverSEL7, driverSTART8);
+	static Button driverRTrigger = new TriggerButton(driver, Hand.kRight);
+	static Button driverStartSelect = new DoubleButton(driverSEL7, driverSTART8);
 
 	// Buttons for coDriver
-	XboxController coDriver = new XboxController(1);
+	static XboxController coDriver = new XboxController(1);
 	Button coDriverA1 = new JoystickButton(coDriver, 1);
 	Button coDriverB2 = new JoystickButton(coDriver, 2);
 	Button coDriverX3 = new JoystickButton(coDriver, 3);
 	Button coDriverY4 = new JoystickButton(coDriver, 4);
 	Button coDriverLB5 = new JoystickButton(coDriver, 5);
 	Button coDriverRB6 = new JoystickButton(coDriver, 6);
-	Button coDriverSEL7 = new JoystickButton(coDriver, 7);
-	Button coDriverSTART8 = new JoystickButton(coDriver, 8);
+	static Button coDriverSEL7 = new JoystickButton(coDriver, 7);
+	static Button coDriverSTART8 = new JoystickButton(coDriver, 8);
 	Button coDriverLS9 = new JoystickButton(coDriver, 9);
 	Button coDriverRS10 = new JoystickButton(coDriver, 10);
 	Button coDriverDLeft = new DPadButton(coDriver, DPadButton.Value.kDPadLeft);
@@ -46,7 +46,7 @@ public class OI {
 	Button coDriverDRight = new DPadButton(coDriver, DPadButton.Value.kDPadRight);
 	Button coDriverLTrigger = new TriggerButton(coDriver, Hand.kLeft);
 	Button coDriverRTrigger = new TriggerButton(coDriver, Hand.kRight);
-	Button coDriverStartSelect = new DoubleButton(coDriverSEL7, coDriverSTART8);
+	static Button coDriverStartSelect = new DoubleButton(coDriverSEL7, coDriverSTART8);
 
 	public static boolean isConfigured = false;
 
@@ -123,7 +123,12 @@ public class OI {
 	public boolean isDriverRightStickPressed() {
 		return driverRS10.get();
 	}
-	public boolean isDriverRTriggerPressed() {
+
+	public static boolean isDriverRTriggerPressed() {
 		return driverRTrigger.get();
+	}
+
+	public static boolean isCancelledPressed(){
+		return driverStartSelect.get() || coDriverStartSelect.get();
 	}
 }
--- src/main/java/frc/robot/commands/AutoRocketFar.java
+++ src/main/java/frc/robot/commands/AutoRocketFar.java
@@ -3,15 +3,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.motionProfiles.MotionProfiles;
+import frc.robot.OI;
 
 public class AutoRocketFar extends CommandGroup {
 	public AutoRocketFar(boolean mirrored) {
+		this.setInterruptible(false);
 		addSequential(new GyroReset());
 		addParallel(new HatchGrabberExtend());
 		addParallel(new HatchArmExtendDelayed(1));
 		addSequential(new DriveMotionProfile("backwards_to_far_rocket", mirrored));
 		addParallel(new ElevatorToTop());
-		addSequential(new DriveMotionProfile(MotionProfiles.generate1DPF(3.2, 5, 3, 100, false)));
+		addSequential(new DriveMotionProfile(MotionProfiles.generate1DPF(3.2, 6, 4, 100, false)));
 		addSequential(new DriveAutoConfirm());
 		addSequential(new HatchGrabberRetract());
 		addSequential(new WaitCommand(0.25));
@@ -23,5 +25,10 @@ public class AutoRocketFar extends CommandGroup {
 		addSequential(new HatchGrabberExtend());
 		addSequential(new WaitCommand(0.25));
 		addSequential(new DriveMotionProfile(MotionProfiles.generate1DPF(7, 16, 12, 300, true)));
+	}
+
+	@Override
+	public boolean isFinished(){
+		return super.isFinished() || OI.isCancelledPressed();
 	}
 }
