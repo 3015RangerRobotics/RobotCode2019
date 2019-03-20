cfd7bd7a6198774fecee39adda51ad348f50b5c2
Kayla Stein <kstein0119@gmail.com>
2019-03-20 17:44:02 -0400

    testing vision things

--- src/main/java/frc/robot/commands/DriveToTarget.java
+++ src/main/java/frc/robot/commands/DriveToTarget.java
@@ -1,5 +1,8 @@
 package frc.robot.commands;
 
+import frc.motionProfiles.MotionProfiles;
+import frc.robot.Robot;
+
 public class DriveToTarget extends CommandBase {
 	public DriveToTarget() {
 
@@ -15,11 +18,12 @@ public class DriveToTarget extends CommandBase {
 		// dx *= -1;
 		// }
 		// System.out.println("End angle: " + (Robot.getVisionAngle1() + Robot.getVisionAngle2()));
-		// System.out.println(Robot.getVisionXDistance() + ", " + Robot.getVisionZDistance());
-		// if (Robot.getVisionDistance() > 0) new DriveMotionProfile(MotionProfiles.generate2DPF(-Robot.getVisionXDistance() / 12, Robot.getVisionZDistance() / 12, Robot.getVisionAngle2(), 5, 3, 100, false)).start();
+		// System.out.println(Robot.getVisionXDistance() + ", " +
+		// Robot.getVisionZDistance());
+		// if (Robot.getVisionDistance() > 0) new DriveMotionProfile(MotionProfiles.generate2DPF(Robot.getVisionZDistance(), -Robot.getVisionXDistance(), 0, Robot.getVisionAngle1(), 5, 3, 100, false)).start();
 			// if(Robot.getVisionDistance() > 0) new DriveToTargetJank(Robot.getVisionAngle1(), Robot.getVisionAngle2(), Robot.getVisionDistance() / 12).start();
 		// new DriveMotionProfile(MotionProfiles.generate1DPF(distanceToTarget, 8, 5, 100, false)).start();
-		// if(Robot.getVisionDistance() > 0) new DriveMotionProfile(MotionProfiles.generate2DToTarget((Robot.getVisionZDistance() / 12), (Robot.getVisionXDistance() / 12), Robot.getVisionAngle1(), Robot.getVisionAngle2(), 8, 4)).start();;
+		if(Robot.getVisionDistance() > 0) new DriveMotionProfile(MotionProfiles.generate2DToTarget(Robot.getVisionZDistance(), Robot.getVisionXDistance(), Robot.getVisionAngle1(), Robot.getVisionAngle2(), 4, 2)).start();;
 	}
 
 	@Override
--- src/main/java/frc/robot/commands/DriveTurnToTarget.java
+++ src/main/java/frc/robot/commands/DriveTurnToTarget.java
@@ -1,14 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 
 public class DriveTurnToTarget extends CommandBase {
 
 	@Override
 	protected void initialize() {
-		double targetXAngle = SmartDashboard.getNumber("TargetXAngle", -1);
-		double targetYAngle = SmartDashboard.getNumber("TargetYAngle", -1);
-		if (targetXAngle == -1 && targetYAngle == -1) {
+		double targetXAngle = Robot.getVisionAngle1(); // SmartDashboard.getNumber("TargetXAngle", -1);
+		if (targetXAngle == -1) {
 			System.out.println("No target");
 		} else {
 			new DriveTurnToAngleWithEncoders(targetXAngle, 14, 10).start();
